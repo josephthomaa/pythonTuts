9cd25fc5895a964f618546c31820b1d274837f73
simelabs <josephthomaa@gmail.com>
2019-09-16 15:29:01 +0530

    changes

--- sendmail.py
+++ sendmail.py
@@ -14,7 +14,7 @@ body = "Hello from python"
 msg.attach(MIMEText(body, 'plain'))
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
-server.login(fromaddr, "mondaylv")
+server.login(fromaddr, "")
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
--- tictac/main.py
+++ tictac/main.py
@@ -1,8 +1,12 @@
 game = [[0,0,0],[0,0,0],[0,0,0]]
 
-print("   0  1  2")
-count = 0
+def game_board(game_map,player=0, row=0, column=0, just_display=False):
+	print("   a  b  c")
+	if not just_display:
+		game[row][count] = player
+	for count, row in enumerate(game_map):
+		print(count, row)
+	return game_map	
 
-for count, row in enumerate(game):
-	print(count, row)
-	
+game = game_board(game,just_display=True)	
+
