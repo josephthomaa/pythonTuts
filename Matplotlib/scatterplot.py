573c73c641f410a4731bf4b8d62e62bb270987ff
simelabs <josephthomaa@gmail.com>
2019-09-30 18:35:45 +0530

    ml

--- Matplotlib/scatterplot.py
+++ Matplotlib/scatterplot.py
@@ -11,4 +11,12 @@ insurance_filepath = "insurance.csv"
 # Read the file into a variable insurance_data
 #insurance_data = pd.read_csv("data/insurance.csv")
 
-insurance_data = pd.read_csv('data/insurance.csv')
+insurance_data = pd.read_csv('Matplotlib/data/insurance.csv')
+
+print(insurance_data.head())
+sns.scatterplot(x=insurance_data['bmi'], y=insurance_data['charges'], hue=insurance_data['smoker'])
+#sns.regplot(x=insurance_data['bmi'], y=insurance_data['charges'])
+sns.lmplot(x="bmi", y="charges", hue="smoker", data=insurance_data)
+sns.swarmplot(x=insurance_data['smoker'],
+              y=insurance_data['charges'])
+plt.show()
